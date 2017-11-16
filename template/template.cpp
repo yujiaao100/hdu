1cfaff3c190d8af315029f6d38e9b7e9438a6df7
test <test@rayjoy.com>
2017-11-16 10:56:54 +0800

    update

---
+++ template/template.cpp
@@ -0,0 +1,14 @@
+/*
+  Name:
+  Copyright:
+  Author:yujiaao
+  Date:
+  Description:hdu
+*/
+#include<cstdio>
+#include<cstring>
+int main()
+{
+	//freopen("../test/in.txt","r",stdin);
+	//freopen("../test/out.txt","w",stdout);
+}
