e61854f784a22bff67045b29841c6b0e534a2dea
test <test@rayjoy.com>
2017-11-16 11:02:11 +0800

    update

---
+++ hdu1000.cpp
@@ -0,0 +1,19 @@
+/*
+  Name:
+  Copyright:
+  Author:yujiaao
+  Date:
+  Description:hdu1001
+*/
+#include<cstdio>
+#include<cstring>
+int main()
+{
+	//freopen("../test/in.txt","r",stdin);
+	//freopen("../test/out.txt","w",stdout);
+	int a,b;
+	while(scanf("%d %d",&a,&b)!=EOF)
+	{
+			printf("%d\n",a+b);
+	}
+}
